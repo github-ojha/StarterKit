ac29f004177d2407952f33004f02d257a0c943ca
Neeraj Ojha <neeraj.ojha@vbrick.com>
2016-11-27 06:21:47 +0530

    bundle splitting

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -6,14 +6,15 @@ export default {
   debug: true,
   devtool: 'source-map', //for production use source-map instead is inline-source-map as it is more powerfull, suited for prod env
   noInfo: false,
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry:{
+    main: path.resolve(__dirname, 'src/index'),
+    vendor: path.resolve(__dirname, 'src/vendor'),
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'), //output to be generated in dist folder instead of src
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   plugins: [
     // create html file dynamically that has ref to bundle.js
@@ -32,6 +33,11 @@ export default {
         minifyURLs: true
       },
       inject: true,
+    }),
+    // Use CommonsChunkPlugin to create a separate bundle
+    // of vendor libraries so that they're cached separately.
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
     }),
     //eliminate duplicagte packages when creating bundle
     new webpack.optimize.DedupePlugin(),
